8d0161dbad1df113737e6cdb73e45742f7261753
Vladyslav Tereshkevych <vladyslav_tereshkevych@epam.com>
2019-08-20 19:46:33 +0300

    Created field validation.

--- src/main/java/com/epam/bookshop/controller/BookController.java
+++ src/main/java/com/epam/bookshop/controller/BookController.java
@@ -1,13 +1,17 @@
 package com.epam.bookshop.controller;
 
+import com.epam.bookshop.exception.BadRequestException;
 import com.epam.bookshop.model.Book;
 import com.epam.bookshop.model.Views;
 import com.epam.bookshop.service.BookService;
+import com.epam.bookshop.util.ErrorExtractor;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,7 +23,8 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Book save(@RequestBody Book book) {
+    public Book save(@Valid @RequestBody Book book, BindingResult bindingResult) {
+        validate(bindingResult);
         return bookService.save(book);
     }
 
@@ -36,7 +41,8 @@ public class BookController {
     }
 
     @PutMapping("{id}")
-    public Book update(@PathVariable Long id, @RequestBody Book book) {
+    public Book update(@PathVariable Long id, @Valid @RequestBody Book book, BindingResult bindingResult) {
+        validate(bindingResult);
         book.setId(id);
         return bookService.update(book);
     }
@@ -45,5 +51,11 @@ public class BookController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") Book book) {
         bookService.delete(book);
+    }
+
+    private void validate(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new BadRequestException(ErrorExtractor.getErrors(bindingResult));
+        }
     }
 }
--- src/main/java/com/epam/bookshop/model/Book.java
+++ src/main/java/com/epam/bookshop/model/Book.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.annotation.JsonView;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -26,6 +29,7 @@ public class Book {
     @JsonView(Views.Public.class)
     private Long id;
 
+    @NotBlank(message = "Name cannot be empty")
     @JsonView(Views.Public.class)
     private String name;
 
@@ -34,6 +38,8 @@ public class Book {
     @JsonView(Views.Public.class)
     private Genre genre;
 
+    @DecimalMin(value = "0", message = "Price cannot be less than 0")
+    @NotNull(message = "Price cannot be empty")
     @JsonView(Views.Public.class)
     private BigDecimal price;
 
