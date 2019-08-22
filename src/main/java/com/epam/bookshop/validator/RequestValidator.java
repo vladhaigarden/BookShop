78fd400c3a3ccae73de0b1e899fad08eadfd0e02
Vladyslav Tereshkevych <vladyslav_tereshkevych@epam.com>
2019-08-22 17:27:57 +0300

    Created RequestValidator.

---
+++ src/main/java/com/epam/bookshop/validator/RequestValidator.java
@@ -0,0 +1,18 @@
+package com.epam.bookshop.validator;
+
+import com.epam.bookshop.exception.BadRequestException;
+import com.epam.bookshop.util.ErrorExtractor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public final class RequestValidator {
+
+    public void validateRequest(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new BadRequestException(ErrorExtractor.getErrors(bindingResult));
+        }
+    }
+}
